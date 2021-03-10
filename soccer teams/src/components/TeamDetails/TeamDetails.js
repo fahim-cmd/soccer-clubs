8547c539503677a20c454c25e8a9deedcf129502
fahim-cmd <hfahim087@gmail.com>
2021-03-10 19:17:04 +0600

    added readme info for bonus

--- soccer teams/src/components/TeamDetails/TeamDetails.js
+++ soccer teams/src/components/TeamDetails/TeamDetails.js
@@ -4,13 +4,12 @@ import '../TeamDetails/TeamDetails.css';
 import TeamInfo from '../TeamInfo/TeamInfo';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFacebook, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons"
-import { Link } from 'react-router-dom';
 
 
 const TeamDetails = () => {
     const { teamId } = useParams();
     const [teamDetails, setTeamDetails] = useState({})
-
+    console.log(teamDetails)
     useEffect(() => {
         const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${teamId}
         `;
@@ -19,7 +18,7 @@ const TeamDetails = () => {
             .then(data => setTeamDetails(data.teams[0]))
     }, [])
 
-        const {strFacebook, strYoutube, strTwitter } = teamDetails;
+    const { strFacebook, strYoutube, strTwitter } = teamDetails;
     return (
         <div style={{ backgroundColor: "#fb743e" }}>
             <div className="stadium" >
@@ -27,7 +26,7 @@ const TeamDetails = () => {
             </div>
 
             <div className="container">
-                <div className="row">
+                <div>
                     {
                         <TeamInfo teamDetails={teamDetails}></TeamInfo>
                     }
@@ -35,19 +34,18 @@ const TeamDetails = () => {
             </div>
 
             <div className="container" style={{ color: "white" }}>
-                <p >
+                <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum doloremque atque voluptas pariatur, architecto minus mollitia voluptatem aspernatur odit amet officiis magni ea nemo officia sequi consectetur tenetur itaque repellat odio neque perferendis nesciunt ab est! Consectetur nesciunt nulla laudantium nisi eveniet obcaecati at autem commodi itaque ipsam id porro, architecto sed quisquam! Enim quos doloremque aliquam accusantium, nobis animi vel quibusdam cum assumenda eos minus ducimus error incidunt placeat magni repudiandae. Adipisci molestias enim, incidunt hic veritatis distinctio odit, neque eligendi voluptas labore, pariatur reprehenderit iure perspiciatis. Quasi repudiandae a asperiores dolor harum necessitatibus obcaecati, eum tempora sint?
                 </p>
                 <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta placeat vel rerum delectus expedita enim fugiat labore. Ipsum rem temporibus obcaecati voluptatum excepturi! Odio temporibus, ad, modi distinctio quisquam delectus sint molestiae, rem ipsam nesciunt sit placeat voluptas doloribus illum nisi aperiam pariatur fugiat ab necessitatibus! Aut quia accusamus maiores possimus voluptatum iste quaerat corrupti quam, animi aliquam ea neque saepe, vero molestiae beatae repellat hic consectetur. Aliquam magni at amet obcaecati? Architecto nemo adipisci fugit quasi, doloribus at aspernatur nihil inventore maxime, consectetur repellendus aperiam recusandae aliquid ipsum blanditiis dolorem quo aliquam deleniti voluptas? Ex reiciendis est quos iusto?
-                  
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta placeat vel rerum delectus expedita enim fugiat labore. Ipsum rem temporibus obcaecati voluptatum excepturi! Odio temporibus, ad, modi distinctio quisquam delectus sint molestiae, rem ipsam nesciunt sit placeat voluptas doloribus illum nisi aperiam pariatur fugiat ab necessitatibus! Aut quia accusamus maiores possimus voluptatum iste quaerat corrupti quam, animi aliquam ea neque saepe, vero molestiae beatae repellat hic consectetur. Aliquam magni at amet obcaecati? Architecto nemo adipisci fugit quasi, doloribus at aspernatur nihil inventore maxime, consectetur repellendus aperiam recusandae aliquid ipsum blanditiis dolorem quo aliquam deleniti voluptas? Ex reiciendis est quos iusto?
                 </p>
             </div>
 
             <footer className="container row justify-content-center my-4 mx-4 py-5 ">
-                <Link to={strFacebook}><FontAwesomeIcon className="socialIcon" icon={faFacebook} /> </Link>
-                <Link to={strYoutube}> <FontAwesomeIcon className="socialIcon" icon={faYoutube} /></Link>
-                <Link to={strTwitter}><FontAwesomeIcon className="socialIcon" icon={faTwitter} /> </Link> 
+                <a href={strFacebook}><FontAwesomeIcon className="socialIcon" icon={faFacebook} /> </a>
+                <a href={strYoutube}> <FontAwesomeIcon className="socialIcon" icon={faYoutube} /></a>
+                <a href={strTwitter}><FontAwesomeIcon className="socialIcon" icon={faTwitter} /> </a>
             </footer>
         </div>
     );
--- soccer teams/src/components/TeamInfo/TeamInfo.js
+++ soccer teams/src/components/TeamInfo/TeamInfo.js
@@ -8,23 +8,22 @@ const TeamInfo = (props) => {
     const { strTeam, strGender, strCountry, intFormedYear, strSport } = props.teamDetails;
 
     let picture;
-    if (strGender === "Male") {
+
+    strGender === "Male" ?
         picture = <img className="w-100 h-100 rounded" src={male} alt="" />
-    }
-    else {
-        picture = <img className="w-100 h-100 rounded" src={female} alt="" />
-    }
+
+        : picture = <img className="w-100 h-100 rounded" src={female} alt="" />
 
     return (
-        <div style={{ backgroundColor: '#f9f3f3' }} className="d-flex justify-content-between my-5 p-4 rounded">
-            <div className="col-md-8">
+        <div style={{ backgroundColor: '#f9f3f3' }} className="row d-flex justify-content-between my-5 p-4 rounded">
+            <div className="col-12 col-sm-8 col-md-8">
                 <h1>{strTeam} {strGender} </h1>
                 <h5> <FontAwesomeIcon icon={faPodcast} />Founded: {intFormedYear}</h5>
                 <h5> <FontAwesomeIcon icon={faFlag} />Country: {strCountry}</h5>
                 <h5> <FontAwesomeIcon icon={faFutbol} /> Sport Type: {strSport}</h5>
                 <h5> <FontAwesomeIcon icon={faMars} /> Gender: {strGender}</h5>
             </div>
-            <div className="col-md-4">
+            <div className="col-12 col-sm-8 col-md-4">
                 {
                     picture
                 }
